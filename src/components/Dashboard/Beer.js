22058a33d066a7e9a552d5967d659d36370d71da
santiagosossa <santiagosossa95@gmail.com>
2019-11-01 15:12:29 -0500

    [feat] upload beer image
    added upload beer img and some SCSS changes in dashboard

--- src/components/Dashboard/Beer.js
+++ src/components/Dashboard/Beer.js
@@ -3,10 +3,16 @@ import React from 'react'
 export default function Beer(props) {
     return(
         <div className="beer" id="">
+            <div className="beerPhoto">
+                <img src="{props.photo}" alt=""/>
+            </div>
+            <div className="beerInfo">
                 <h1>Name: {props.name}</h1>
                 <h1>Country: {props.country}</h1>
                 <h1>Alcohol: {props.alcohol}%</h1>
-                <h1>{props.photo}</h1>
-        </div>
+                <h1>Rating: {props.rating}</h1>
+                <h1>IBU: {props.IBU}</h1>
+            </div>
+        </div>  
     )
 }
